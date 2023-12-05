74b242c33cb5fca249d6611acc0fca95369eb802
Luke de Freitas <ashleydeep@gmail.com>
2023-12-05 18:19:16 +0200

    initial setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = new string[4];
+
+input[0] = "1abc2";
+input[1] = "pqr3stu8vwx";
+input[2] = "a1b2c3d4e5f";
+input[3] = "treb7uchet";
+
+foreach (var line in input)
+{
+    Console.WriteLine(line);
+}
